2156b894509a3ed218089018ca1d74b3a6529a69
jaime <jaimerecovery23@gmail.com>
2023-12-23 18:07:30 +0100

    Added player entity

--- src/main/java/com/example/WeBKKHustraUhrovec/Entity/Player.java
+++ src/main/java/com/example/WeBKKHustraUhrovec/Entity/Player.java
@@ -2,8 +2,7 @@ package com.example.WeBKKHustraUhrovec.Entity;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 @Entity
 @Table(name="player")
@@ -15,19 +14,23 @@ public class Player {
     private int playerID;
 
     @Column(name = "player_name", length = 255)
-    @NotEmpty
+    @NotEmpty(message = "Name must not be empty")
+    @Pattern(regexp = "^[a-zA-ZščťžýáíéúäôöüěřščřžýáíéúäôöüěřščřžľďťňĺŕŠČŤŽÝÁÍÉÚÄÔÖÜĚŘŠČŘŽĹĽ]+$", message = "Name can only contain letters and special characters")
     private String name;
 
     @Column(name = "player_surname", length = 255)
-    @NotEmpty
+    @NotEmpty(message = "Surname must not be empty")
+    @Pattern(regexp = "^[a-zA-ZščťžýáíéúäôöüěřščřžýáíéúäôöüěřščřžľďťňĺŕŠČŤŽÝÁÍÉÚÄÔÖÜĚŘŠČŘŽĹĽ]+$", message = "Surname can only contain letters and special characters")
     private String surname;
 
     @Column(name = "player_age", length = 255)
-    @NotEmpty
+    @NotEmpty(message = "Age must not be empty")
+    @Positive(message = "Age must be greater than 0")
     private String age;
 
     @Column(name = "player_points", length = 255)
-    @NotEmpty
+    @NotEmpty(message = "Points must not be empty")
+    @PositiveOrZero(message = "Points must be greater than or equal to 0")
     private String points;
 
 
